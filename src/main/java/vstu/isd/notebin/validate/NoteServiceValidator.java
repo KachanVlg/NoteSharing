ac15d587ef524811cdfb7dd14e6817a20155ba80
sauce-chili <saucechilidev@gmail.com>
2024-11-30 03:41:21 +0300

    feat: added template for validation `UpdateNoteRequestDto`

---
+++ src/main/java/vstu/isd/notebin/validate/NoteServiceValidator.java
@@ -0,0 +1,9 @@
+package vstu.isd.notebin.validate;
+
+import vstu.isd.notebin.dto.UpdateNoteRequestDto;
+
+public class NoteServiceValidator {
+    public void validateUpdateRequest(UpdateNoteRequestDto updateRequest){
+
+    }
+}
